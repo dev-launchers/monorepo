7bcc3dba46a2d0a1162518743c61cc5dba17c98d
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-06-19 20:00:10 +0300

    test

--- packages/UI/src/components/Button/Button.stories.js
+++ packages/UI/src/components/Button/Button.stories.js
@@ -9,7 +9,7 @@ export default {
 export const WithShortText = (args) => <Button {...args}>Button</Button>;
 
 export const WithLongText = (args) => (
-  <Button {...args}>Welcome to Dev Launchers</Button>
+  <Button {...args}>Welcome to Dev Launcherss</Button>
 );
 
 export const Customizable = (args) => <Button {...args}>{args.text}</Button>;
