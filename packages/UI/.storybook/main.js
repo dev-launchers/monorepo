29ba8822dc888f860b7c741464f329a2907fd3fc
Enjoy2Live <iMohammedMaqbol@gmail.com>
2022-08-02 19:04:18 +0300

    fix(Enjoy2Live): convert to vite

--- packages/UI/.storybook/main.js
+++ packages/UI/.storybook/main.js
@@ -1,14 +1,24 @@
+// use `mergeConfig` to recursively merge Vite options
+const { mergeConfig } = require("vite");
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  staticDirs: ['../public'],
-  "addons": [
+  core: { builder: "@storybook/builder-vite" },
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  staticDirs: ["../public"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/preset-create-react-app",
-    "storybook-addon-next-router"
+    "storybook-addon-next-router",
   ],
-  "framework": "@storybook/react"
-}
+  framework: "@storybook/react",
+
+  async viteFinal(config, { configType }) {
+    // return the customized config
+    return mergeConfig(config, {
+      // customize the Vite config here
+      resolve: {
+        alias: { foo: "bar" },
+      },
+    });
+  },
+  // ... other options here
+};
