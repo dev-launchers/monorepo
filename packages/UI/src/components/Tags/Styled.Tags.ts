1cb6963c20be14fd512cd8f315f7a3efd0044c5f
mfvil <rarefyev@gmail.com>
2022-10-10 18:29:50 -0500

    feat(adding tags storries): adding tags stories and refactoring the tags component

--- packages/UI/src/components/Tags/Styled.Tags.ts
+++ packages/UI/src/components/Tags/Styled.Tags.ts
@@ -1,33 +1,69 @@
 import styled from 'styled-components';
 import { typographyStyles } from '../Typography/Styled.Typography';
+import type { Props } from './Tags';
 
-interface TagsProps {
-  bgColor: string;
-  txtColor: string;
-}
-
-export const Tag = styled.div<TagsProps>`
-  ${typographyStyles['p']}
-  background: ${({ theme, bgColor }) => {
-    if (bgColor === 'darkGrey') return theme.colors.GREYSCALE_300;
-    else if (bgColor === 'black') return theme.colors.GREYSCALE_BLACK;
-    else if (bgColor === 'white') return theme.colors.GREYSCALE_WHITE;
-    else if (bgColor === 'yellow') return theme.colors.YELLOW_200;
-    else if (bgColor === 'lightBlue') return theme.colors.LIGHTBLUE_200;
-    else if (bgColor === 'blue') return theme.colors.BLUE_200;
-    else if (bgColor === 'orange') return theme.colors.ORANGE_200;
+type StyledProps = Pick<Props, 'bgColor' | 'txtColor'>;
+export const Tag = styled.div<StyledProps>`
+  ${typographyStyles['button']}
+  background-color: ${({ theme, bgColor }) => {
+    if (bgColor === 'GREYSCALE_CHARCOAL')
+      return theme.colors.GREYSCALE_CHARCOAL;
+    else if (bgColor === 'GREYSCALE_BLACK') return theme.colors.GREYSCALE_BLACK;
+    else if (bgColor === 'GREYSCALE_WHITE') return theme.colors.GREYSCALE_WHITE;
+    else if (bgColor === 'YELLOW_200') return theme.colors.YELLOW_200;
+    else if (bgColor === 'LIGHT_BLUE_200') return theme.colors.LIGHT_BLUE_200;
+    else if (bgColor === 'BLUE_200') return theme.colors.BLUE_200;
+    else if (bgColor === 'ORANGE_200') return theme.colors.ORANGE_200;
   }};
   color: ${({ theme, txtColor }) => {
-    if (txtColor === 'black') return theme.colors.GREYSCALE_300;
-    else if (txtColor === 'white') return theme.colors.GREYSCALE_WHITE;
+    if (txtColor === 'GREYSCALE_BLACK') return theme.colors.GREYSCALE_BLACK;
+    else if (txtColor === 'GREYSCALE_WHITE')
+      return theme.colors.GREYSCALE_WHITE;
   }};
   border-radius: 2rem;
+  max-width: 7.9375rem;
+  text-align: center;
+  padding: 0.5rem 1rem;
 `;
 export const CloseButton = styled.button`
   ${typographyStyles['button']}
+  background-color: ${({ theme, bgColor }) => {
+    if (bgColor === 'GREYSCALE_CHARCOAL')
+      return theme.colors.GREYSCALE_CHARCOAL;
+    else if (bgColor === 'GREYSCALE_BLACK') return theme.colors.GREYSCALE_BLACK;
+    else if (bgColor === 'GREYSCALE_WHITE') return theme.colors.GREYSCALE_WHITE;
+    else if (bgColor === 'YELLOW_200') return theme.colors.YELLOW_200;
+    else if (bgColor === 'LIGHT_BLUE_200') return theme.colors.LIGHT_BLUE_200;
+    else if (bgColor === 'BLUE_200') return theme.colors.BLUE_200;
+    else if (bgColor === 'ORANGE_200') return theme.colors.ORANGE_200;
+  }};
+  color: ${({ theme, txtColor }) => {
+    if (txtColor === 'GREYSCALE_BLACK') return theme.colors.GREYSCALE_BLACK;
+    else if (txtColor === 'GREYSCALE_WHITE')
+      return theme.colors.GREYSCALE_WHITE;
+  }};
+  width: 18%;
+  height: 18%;
+  border: none;
+  margin-left: 0.9375rem;
+  cursor: pointer;
 `;
 export const CloseIcon = styled.svg`
-  color: ${({ theme }) => theme.colors.GREYSCALE_WHITE};
+  background-color: ${({ theme, bgColor }) => {
+    if (bgColor === 'GREYSCALE_CHARCOAL')
+      return theme.colors.GREYSCALE_CHARCOAL;
+    else if (bgColor === 'GREYSCALE_BLACK') return theme.colors.GREYSCALE_BLACK;
+    else if (bgColor === 'GREYSCALE_WHITE') return theme.colors.GREYSCALE_WHITE;
+    else if (bgColor === 'YELLOW_200') return theme.colors.YELLOW_200;
+    else if (bgColor === 'LIGHT_BLUE_200') return theme.colors.LIGHT_BLUE_200;
+    else if (bgColor === 'BLUE_200') return theme.colors.BLUE_200;
+    else if (bgColor === 'ORANGE_200') return theme.colors.ORANGE_200;
+  }};
+  color: ${({ theme, txtColor }) => {
+    if (txtColor === 'GREYSCALE_BLACK') return theme.colors.GREYSCALE_BLACK;
+    else if (txtColor === 'GREYSCALE_WHITE')
+      return theme.colors.GREYSCALE_WHITE;
+  }};
   height: 100%;
   width: 100%;
 `;
--- packages/UI/src/components/Tags/Tags.stories.tsx
+++ packages/UI/src/components/Tags/Tags.stories.tsx
@@ -1,4 +1,4 @@
-import { ComponentStory, ComponentMeta } from '@storybook/react';
+import type { ComponentStory, ComponentMeta } from '@storybook/react';
 import { within, userEvent, waitFor } from '@storybook/testing-library';
 import * as React from 'react';
 import Tags from './Tags';
@@ -8,13 +8,69 @@ export default {
   component: Tags,
 } as ComponentMeta<typeof Tags>;
 
-const TemplateTags: ComponentStory<typeof Tags> = (args) => (
+const FilterTag: ComponentStory<typeof Tags> = (args) => (
+  <Tags {...args} isFilter>
+    Tags
+  </Tags>
+);
+const RegularTag: ComponentStory<typeof Tags> = (args) => (
   <Tags {...args}>Tags</Tags>
 );
 
-export const Grey = TemplateTags.bind({});
+export const Black = RegularTag.bind({});
+Black.args = { ...Black.args };
+
+export const Grey = FilterTag.bind({});
 Grey.args = { ...Grey.args };
 
+export const GreyFilter = RegularTag.bind({});
+GreyFilter.args = { ...GreyFilter.args };
+
+export const White = RegularTag.bind({});
+White.args = { ...White.args };
+
+export const Yellow = RegularTag.bind({});
+Yellow.args = { ...Yellow.args };
+
+export const LightBlue = RegularTag.bind({});
+LightBlue.args = { ...LightBlue.args };
+
+export const Blue = RegularTag.bind({});
+Blue.args = { ...Blue.args };
+
+export const Orange = RegularTag.bind({});
+Orange.args = { ...Orange.args };
+
+Black.args = {
+  bgColor: 'GREYSCALE_BLACK',
+  txtColor: 'GREYSCALE_WHITE',
+};
+
 Grey.args = {
-  bgColor: 'darkGrey',
+  bgColor: 'GREYSCALE_CHARCOAL',
+  txtColor: 'GREYSCALE_WHITE',
+};
+GreyFilter.args = {
+  bgColor: 'GREYSCALE_CHARCOAL',
+  txtColor: 'GREYSCALE_WHITE',
+};
+White.args = {
+  bgColor: 'GREYSCALE_WHITE',
+  txtColor: 'GREYSCALE_BLACK',
+};
+Yellow.args = {
+  bgColor: 'YELLOW_200',
+  txtColor: 'GREYSCALE_WHITE',
+};
+LightBlue.args = {
+  bgColor: 'LIGHT_BLUE_200',
+  txtColor: 'GREYSCALE_WHITE',
+};
+Blue.args = {
+  bgColor: 'BLUE_200',
+  txtColor: 'GREYSCALE_WHITE',
+};
+Orange.args = {
+  bgColor: 'ORANGE_200',
+  txtColor: 'GREYSCALE_WHITE',
 };
