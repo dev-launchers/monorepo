0532aa216058ec0748e4f4c5e32d79d81720cb32
FirdaousseErrazi <firdaousse1995@gmail.com>
2022-10-12 21:44:13 +0000

    feat(components): added Raius styles

---
+++ packages/UI/src/components/Radius/StyledRadius.ts
@@ -0,0 +1,37 @@
+import { css } from 'styled-components';
+
+const radius100 = css`
+  border-radius: 4px;
+`;
+const radius200 = css`
+  border-radius: 8px;
+`;
+const radius300 = css`
+  border-radius: 16px;
+`;
+const radius400 = css`
+  border-radius: 24px;
+`;
+const radius500 = css`
+  border-radius: 32px;
+`;
+const radius600 = css`
+  border-radius: 48px;
+`;
+const radius700 = css`
+  border-radius: 64px;
+`;
+const radius800 = css`
+  border-radius: 96px;
+`;
+
+export const radiusStyles = {
+  radius100,
+  radius200,
+  radius300,
+  radius400,
+  radius500,
+  radius600,
+  radius700,
+  radius800,
+};
