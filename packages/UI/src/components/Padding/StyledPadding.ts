9cf4c5d61b9d388c3cefbb45b4935d74f51cf901
FirdaousseErrazi <firdaousse1995@gmail.com>
2022-10-12 21:40:36 +0000

    feat(components): added Padding styles

---
+++ packages/UI/src/components/Padding/StyledPadding.ts
@@ -0,0 +1,65 @@
+import { css } from 'styled-components';
+
+const uniPad100 = css`
+  padding: 4px;
+`;
+const uniPad200 = css`
+  padding: 8px;
+`;
+const uniPad300 = css`
+  padding: 16px;
+`;
+const uniPad400 = css`
+  padding: 24px;
+`;
+const uniPad500 = css`
+  padding: 32px;
+`;
+const uniPad600 = css`
+  padding: 48px;
+`;
+const uniPad700 = css`
+  padding: 64px;
+`;
+const uniPad800 = css`
+  padding: 96px;
+`;
+const uniPad900 = css`
+  padding: 128px;
+`;
+const comPad100 = css`
+  padding: 8px 16px;
+`;
+const comPad200 = css`
+  padding: 12px 24px;
+`;
+const comPad300 = css`
+  padding: 16px 32px;
+`;
+const comPad400 = css`
+  padding: 24px 48px;
+`;
+const comPad500 = css`
+  padding: 48px 64px;
+`;
+const comPad600 = css`
+  padding: 64px 128px;
+`;
+
+export const paddingStyles = {
+  uniPad100,
+  uniPad200,
+  uniPad300,
+  uniPad400,
+  uniPad500,
+  uniPad600,
+  uniPad700,
+  uniPad800,
+  uniPad900,
+  comPad100,
+  comPad200,
+  comPad300,
+  comPad400,
+  comPad500,
+  comPad600,
+};
